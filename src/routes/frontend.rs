7b245f5638dde26a74e57be50b28d22693af250f
Valdir <valdyrtorres@yahoo.com.br>
2026-08-16 17:08:16 -0300

    feat: modelagem usuario e autenticacao

---
+++ src/routes/frontend.rs
@@ -0,0 +1,61 @@
+use askama::Template;
+use axum::{
+    Form, Router,
+    response::{Html, IntoResponse, Redirect, Response},
+    routing::get,
+};
+use axum_extra::extract::{CookieJar, cookie::Cookie};
+use serde::Deserialize;
+
+use crate::{
+    app::AppState,
+    auth::user::{UnauthenticatedUser, User},
+    error::AppError,
+    repository::Repository,
+};
+
+pub fn router() -> Router<AppState> {
+    Router::new()
+        .route("/", get(index))
+        .route("/login", get(login_page).post(login))
+}
+
+#[derive(Template)]
+#[template(path = "login.html")]
+struct LoginPage;
+
+async fn login_page() -> Result<Html<String>, AppError> {
+    let html = LoginPage.render()?;
+    Ok(Html(html))
+}
+
+#[derive(Deserialize)]
+struct LoginForm {
+    username: String,
+    password: String,
+}
+
+async fn login(
+    repository: Repository,
+    jar: CookieJar,
+    Form(request): Form<LoginForm>,
+) -> Result<impl IntoResponse, AppError> {
+    let unauth_user = UnauthenticatedUser::new(request.username, request.password);
+    let user = match unauth_user.authenticate(&repository).await {
+        Ok(user) => user,
+        Err(AppError::UserDoesNotExist) => unauth_user.register(&repository).await?,
+        Err(other_err) => return Err(other_err),
+    };
+
+    let token = user.auth_token()?;
+    let cookie = Cookie::build(("token", token)).http_only(true);
+
+    Ok((jar.add(cookie), Redirect::to("/")))
+}
+
+async fn index(maybe_user: Option<User>) -> Result<Response, AppError> {
+    match maybe_user {
+        Some(user) => Ok(Html(format!("Hello, {}", user.username())).into_response()),
+        None => Ok(Redirect::to("/login").into_response()),
+    }
+}
